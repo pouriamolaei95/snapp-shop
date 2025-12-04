2a4e2f85528d5906c98a4c4c85b6e9276c91dc38
pouriamolaei95 <pouriamolaei95@gmail.com>
2025-12-04 04:05:54 +0330

    Update API base URL configuration to use environment variable for improved flexibility and deployment compatibility.

--- lib/api/config.api.ts
+++ lib/api/config.api.ts
@@ -1,7 +1,9 @@
 import ky from "ky-universal";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+
 export const apiInstance = ky.create({
-  prefixUrl: "http://localhost:3000/api", // TODO: Move to the env file
+  prefixUrl: API_BASE_URL,
   timeout: 30 * 1000, // 30 seconds
   headers: {
     "Content-Type": "application/json",
